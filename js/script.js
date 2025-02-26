8c4a236c8c0c177078dfcf2a9999f9d1fc59f4f9
Eliza <eliza.chistova@gmail.com>
2025-02-26 10:05:10 +0100

    Update Script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const socialIcons = document.querySelectorAll(".social-icons a")
-  
-    socialIcons.forEach((icon) => {
-      icon.addEventListener("mouseover", () => {
-        icon.style.transform = "scale(1.2)"
-      })
-  
-      icon.addEventListener("mouseout", () => {
-        icon.style.transform = "scale(1)"
-      })
+  const socialIcons = document.querySelectorAll(".social-icons a")
+
+  socialIcons.forEach((icon) => {
+    icon.addEventListener("mouseover", () => {
+      icon.style.transform = "scale(1.2)"
     })
-  
-    // Add active class to the current nav item
-    const currentLocation = location.href
-    const menuItems = document.querySelectorAll("nav ul li a")
-    menuItems.forEach((item) => {
-      if (item.href === currentLocation) {
-        item.classList.add("active")
-      }
+
+    icon.addEventListener("mouseout", () => {
+      icon.style.transform = "scale(1)"
     })
-  
-    // Smooth scrolling for table of contents
-    const tocLinks = document.querySelectorAll(".table-of-contents a")
-    tocLinks.forEach((link) => {
-      link.addEventListener("click", function (e) {
-        e.preventDefault()
-        const targetId = this.getAttribute("href")
-        const targetElement = document.querySelector(targetId)
-        targetElement.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        })
+  })
+
+  // Add active class to the current nav item
+  const currentLocation = location.href
+  const menuItems = document.querySelectorAll("nav ul li a")
+  menuItems.forEach((item) => {
+    if (item.href === currentLocation) {
+      item.classList.add("active")
+    }
+  })
+
+  // Smooth scrolling for table of contents
+  const tocLinks = document.querySelectorAll(".table-of-contents a")
+  tocLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault()
+      const targetId = this.getAttribute("href")
+      const targetElement = document.querySelector(targetId)
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
       })
     })
   })
+
+  // Typing effect
+  const typingText = document.getElementById("typing-text");
+  const textToType = "Hello 👋, I'm Eliza!";
+  let i = 0;
+
+  function typeWriter() {
+    if (i < textToType.length) {
+      typingText.innerHTML += textToType.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
+    }
+  }
+
+  if (typingText) {
+    typeWriter();
+  }
+})
